1d94aa8cb15816f1ab599c264685d215ed8ed3c8
nepia11 <kapibara19@gmail.com>
2021-10-13 20:27:03 +0900

    show hierarchy実装

--- __init__.py
+++ __init__.py
@@ -5,8 +5,7 @@
 
 
 import bpy
-import rna_keymap_ui
-from bl_i18n_utils.settings import LANGUAGES
+import pprint
 
 bl_info = {
     "name": "Armature Validator",
@@ -21,27 +20,83 @@ bl_info = {
 }
 
 
-class TEST_Operator(bpy.types.Operator):
+def is_top_level_bone(bone: bpy.types.Bone):
+    """親のいないボーンを探す"""
+    if bone.parent is None:
+        return True
+    else:
+        return False
 
-    bl_idname = "object.hoge"
-    bl_label = "operator label"
 
-    def execute(self, context):
+def search_bone(bone: bpy.types.Bone):
+    """
+    ボーンの子を再帰的に探査する
+    {bone_name.bone_name.bone_name....}みたいなつながりで返すようにしたい
+    """
+    children = list(bone.children)
+    if children == []:
+        return {bone.name: None}
+    else:
+        children_dict = {}
+        for child in children:
+            child_dict = search_bone(child)
+            children_dict.update(child_dict)
+        bone_dict = {bone.name: children_dict}
+        return bone_dict
+
+
+def armature_to_dict(armature: bpy.types.Armature):
+    top_level_bones = [b for b in armature.bones if is_top_level_bone(b)]
+    hierarchy = {}
+    for bone in top_level_bones:
+        tree = search_bone(bone)
+        hierarchy.update(tree)
+    return hierarchy
+
+
+class Show_Armature_Hierarchy_Operator(bpy.types.Operator):
+
+    bl_idname = "armature.show_hierarchy"
+    bl_label = "show hierarchy"
+    bl_options = {"REGISTER", "UNDO"}
+
+    @classmethod
+    def poll(cls, context):
+        try:
+            if context.active_object.type == "ARMATURE":
+                return True
+            return False
+        except AttributeError:
+            return False
+
+    def execute(self, context: bpy.context):
+        obj = context.active_object
+        if obj.type == "ARMATURE":
+            armature: bpy.types.Armature = obj.data
+            armature_dict = armature_to_dict(armature)
+            msg = pprint.pformat(armature_dict, compact=True)
+            self.report({"INFO"}, msg)
         return {"FINISHED"}
 
 
+def menu_fn(self, context):
+    self.layout.operator("armature.show_hierarchy")
+
+
 # Registration
-classes = [TEST_Operator]
+classes = [Show_Armature_Hierarchy_Operator]
 
 
 def register():
     for c in classes:
         bpy.utils.register_class(c)
+    bpy.types.VIEW3D_MT_object_context_menu.append(menu_fn)
 
 
 def unregister():
     for c in classes:
         bpy.utils.unregister_class(c)
+    bpy.types.VIEW3D_MT_object_context_menu.remove(menu_fn)
 
 
 if __name__ == "__main__":
